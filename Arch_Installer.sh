516e63cf5f7da572764b3935ebee72a79b4e0e8a
Elliot Swaim <swaimell@grinnell.edu>
2022-08-12 13:49:06 -0500

    changes

--- Arch_Installer.sh
+++ Arch_Installer.sh
@@ -1,4 +1,39 @@
 #!/bin/bash
 
+##############################################
+# Save Progress
+##############################################
+progress_file_name="Arch_Installer_Progress.txt"
+declare -A PROGRESS_ARRAY
+PROGRESS_ARRAY=(
+    [verify_boot]=0
+)
+
+# Save the current progress
+save()
+{
+    declare -p PROGRESS_ARRAY > $progress_file_name
+    echo $(sed -e "s/-A/-Ag/g" $progress_file_name) > $progress_file_name
+}
+
+load_progress()
+{
+    eval $(cat "./$progress_file_name")
+}
+
+init_progress_file()
+{
+    if [ ! -e "./$progress_file_name" ]
+    then
+        save
+    fi
+}
 
 
+##############################################
+# Main
+##############################################
+init_progress_file
+load_progress
+declare -p PROGRESS_ARRAY
+        
